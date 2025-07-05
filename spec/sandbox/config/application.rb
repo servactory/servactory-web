a83ddc048165082e5014abb0b81e2d30ff890d0b
Anton Sokolov <profox.rus@gmail.com>
2025-07-06 01:01:36 +0700

    Revert "Update application to selectively load Rails frameworks"
    This reverts commit 5035695881a90e51c1430c5b7909dcd51da22a0b.

--- spec/sandbox/config/application.rb
+++ spec/sandbox/config/application.rb
@@ -2,18 +2,7 @@
 
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -36,5 +25,7 @@ module Sandbox
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.enabled = false
   end
 end
