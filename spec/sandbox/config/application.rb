cd913180eac3ef73f3a4e3e1099d0d0f3f613fcb
Anton Sokolov <profox.rus@gmail.com>
2025-07-06 00:41:58 +0700

    Disable ActiveRecord and clean up database configuration
    - Set `config.active_record.enabled` to `false` in the Rails application configuration, disabling ActiveRecord support.
    - Commented out all configurations in `database.yml`, including development, test, and production settings.
    - Cleanup prepares the sandbox app for use cases without a database.

--- spec/sandbox/config/application.rb
+++ spec/sandbox/config/application.rb
@@ -25,5 +25,7 @@ module Sandbox
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.enabled = false
   end
 end
