6f15cf34e0e40b37695a998a2121287cae79ebc0
Anton Sokolov <profox.rus@gmail.com>
2025-07-06 01:01:47 +0700

    Revert "Update `ApplicationRecord` to use ActiveModel and remove database dependency"
    This reverts commit e5d8adac246e860e6ed36e03c1231a18a7c409df.

--- spec/sandbox/app/models/application_record.rb
+++ spec/sandbox/app/models/application_record.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# class ApplicationRecord < ActiveRecord::Base
-#   primary_abstract_class
-# end
-
-class ApplicationRecord
-  include ActiveModel::Model
-  include ActiveModel::Attributes
+class ApplicationRecord < ActiveRecord::Base
+  primary_abstract_class
 end
