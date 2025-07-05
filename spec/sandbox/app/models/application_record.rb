e5d8adac246e860e6ed36e03c1231a18a7c409df
Anton Sokolov <profox.rus@gmail.com>
2025-07-06 00:53:14 +0700

    Update `ApplicationRecord` to use ActiveModel and remove database dependency
    - Replaced `ActiveRecord::Base` inheritance with `ActiveModel::Model` and `ActiveModel::Attributes`.
    - Commented out previous ActiveRecord configuration.
    - Simplifies the application setup by removing dependency on ActiveRecord in the sandbox environment.

--- spec/sandbox/app/models/application_record.rb
+++ spec/sandbox/app/models/application_record.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+# class ApplicationRecord < ActiveRecord::Base
+#   primary_abstract_class
+# end
+
+class ApplicationRecord
+  include ActiveModel::Model
+  include ActiveModel::Attributes
 end
