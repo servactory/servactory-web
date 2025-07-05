7f3c74cf69fb11a194324374d136b0475d0dbfee
Anton Sokolov <profox.rus@gmail.com>
2025-07-06 01:10:51 +0700

    Conditionally raise missing callback errors for Rails 7.1+
    - Added a version check to enable `raise_on_missing_callback_actions` only for Rails versions 7.1.0 or higher.
    - Ensures compatibility with Rails 7.0.x and maintains functionality for newer versions.
    - Prevents unnecessary errors in configurations using older Rails versions.

--- spec/sandbox/config/environments/test.rb
+++ spec/sandbox/config/environments/test.rb
@@ -51,5 +51,7 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions.
-  config.action_controller.raise_on_missing_callback_actions = true
+  if Gem::Version.new(Rails::VERSION::STRING) >= Gem::Version.new("7.1.0")
+    config.action_controller.raise_on_missing_callback_actions = true
+  end
 end
