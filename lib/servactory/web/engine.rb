69a520fd4c779b7898d6509b17324311c676530e
Anton Sokolov <profox.rus@gmail.com>
2025-08-31 18:27:52 +0900

    Refactor version references and add configuration validation
    - Updated version references in layout file to use `::Servactory::VERSION::STRING` for consistency.
    - Added a new initializer to validate `Servactory::Web` configuration after initialization.
    - Raised an error if the configuration is invalid, displaying all validation errors for easier debugging.
    - Improved maintainability by ensuring stricter validation of configuration attributes.

--- lib/servactory/web/engine.rb
+++ lib/servactory/web/engine.rb
@@ -3,12 +3,22 @@
 module Servactory
   module Web
     class Engine < ::Rails::Engine
+      # TODO: # => ::Servactory (?)
       # isolate_namespace Servactory::Web
 
       config.servactory_web = Servactory::Web::Configuration.new
 
       def self.configure
         yield(config.servactory_web) if block_given?
+      end
+
+      initializer "servactory.web.validate_configuration" do
+        config.after_initialize do
+          unless config.servactory_web.valid?
+            errors = config.servactory_web.errors.full_messages
+            raise "Invalid Servactory Web configuration: #{errors.join(', ')}"
+          end
+        end
       end
 
       initializer "servactory.web.assets" do |app|
