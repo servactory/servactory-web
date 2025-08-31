49774336e76ed5631311ddf1d26941e96befdc0b
Anton Sokolov <profox.rus@gmail.com>
2025-08-31 18:31:16 +0900

    Update Servactory::Web configuration and spec dependencies
    - Added `propshaft/railtie` to the spec setup to ensure full initialization for tests.
    - Adjusted `servactory.rb` initializer to configure `Servactory::Web::Engine`, including setting a mount path and services directory.
    - Rearranged `require` statements in `spec_helper.rb` for consistency.
    - Enhanced configuration flexibility by introducing new attributes like `mount_path` and `app_services_directory`.

--- spec/sandbox/config/initializers/servactory.rb
+++ spec/sandbox/config/initializers/servactory.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-Servactory::Web.configure do |config|
+Servactory::Web::Engine.configure do |config|
+  config.mount_path = "/services"
+
   config.app_name = "My App"
   config.app_url = "https://example.com"
+
+  config.app_services_directory = "app/services"
 end
